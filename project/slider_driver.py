97ab80b1ab0ed8c208687217debd65258cadb0be
OSiG43 <simon.galand43@gmail.com>
2023-01-23 00:03:01 +0100

    Reverse value

--- project/slider_driver.py
+++ project/slider_driver.py
@@ -3,5 +3,5 @@ from flask import current_app
 
 def setValue(slider):
     value = slider.value if slider.value <= slider.max else slider.max
-    print(value)
-    current_app.potentiometer.wiper = value
+    print("Change value:" +str(value))
+    current_app.potentiometer.wiper = 127-value #172-value parce que 0 est la valeur max et 127 la valeur min sur l'amphi
