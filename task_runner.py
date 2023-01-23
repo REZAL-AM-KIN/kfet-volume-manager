c0fc47dc1e4c0d020bd77fbdbbc287d719ccdcd9
OSiG43 <simon.galand43@gmail.com>
2023-01-23 21:53:13 +0100

    Handle errors in tasks

--- task_runner.py
+++ task_runner.py
@@ -10,21 +10,25 @@ from wsgi import app
 
 
 def change_slider_value(automation_id):
-    with app.app_context():
-        # Get the automation for the current hour
-        automation = db.session.query(Automation).get(automation_id)
-        # Get the first slider
-        slider = db.session.query(Slider).first()
-        slider.value = automation.value
-        db.session.commit()
-        setValue(slider)
-        print("Value set to "+str(slider.value))
-
+    print("enter in job")
+    try:
+        with app.app_context():
+            # Get the automation for the current hour
+            automation = db.session.query(Automation).get(automation_id)
+            # Get the first slider
+            slider = db.session.query(Slider).first()
+            slider.value = automation.value
+            db.session.commit()
+            setValue(slider)
+            print("Value set to " + str(slider.value))
+    except:
+        import traceback
+        print(traceback.format_exc())
 
 @schedule.repeat(schedule.every().minutes)
 def schedule_changes():
+    print("reloading automation tasks")
     with app.app_context():
-        print("reloading automation tasks")
         schedule.clear()
         # Get all the changes
         automations = db.session.query(Automation).all()
@@ -32,6 +36,7 @@ def schedule_changes():
         for automation in automations:
             schedule.every().day.at(f"{automation.time}").do(change_slider_value, automation_id=automation.id)
             print("Add routine: value "+str(automation.value)+" at "+automation.time)
+    print("reloading completed")
 
 
 while True:
